48deb5bacf23b9e81293fe31d28b6bce8108c0b6
Tiffany <tgateley22@gmail.com>
2024-08-19 02:25:09 -0500

    Day 29: add if __name__ == " __ main__"

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -16,4 +17,5 @@ def about(station, date):
             "temperature": temperature}
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
